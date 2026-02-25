c8d21453a5e4989801f09077da68708fa9a8c957
Marcel Jung <MarcelJung97@web.de>
2026-02-25 23:00:23 +0100

    fix: remove the user_proj_example from the uprj_netlists and att the eFPGA_CPU top module the icesoc and ibex netlists

--- verilog/rtl/uprj_netlists.v
+++ verilog/rtl/uprj_netlists.v
@@ -21,8 +21,9 @@
     // Assume default net type to be wire because GL netlists don't have the wire definitions
     `default_nettype wire
     `include "gl/user_project_wrapper.v"
-    `include "gl/user_proj_example.v"
 `else
     `include "user_project_wrapper.v"
-    `include "user_proj_example.v"
+    `include "eFPGA_CPU_top.v"
+    `include "icesoc_netlists.vh"
+    `include "ibexcore_netlists.vh"
 `endif
